276cdb2d51c75c5cb1145b2b3d37a4a986e931eb
Otávio De Carli Albuquerque <otavioalbe@gmail.com>
2024-10-11 14:57:08 -0300

    CAIXA de entrada, email selecionado ou não OK

--- js/scriptEmail.js
+++ js/scriptEmail.js
@@ -1,6 +1,7 @@
 const novoEmail = document.getElementById("novoEmail");
 const formularioEmail = document.getElementById("formularioEmail")
 const cancelarEmail = document.getElementById("cancelarEmail")
+const checkboxes = document.querySelectorAll('input[type="checkbox"]')
 
 novoEmail.addEventListener('click', function(){
     formularioEmail.classList.remove('hidden')
@@ -8,4 +9,11 @@ novoEmail.addEventListener('click', function(){
 
 cancelarEmail.addEventListener('click', function(){
     formularioEmail.classList.add('hidden')
+})
+
+checkboxes.forEach((checkbox)=> {
+    checkbox.addEventListener('change', function(){
+        const fila = checkbox.closest('tr');
+        checkbox.checked ? fila.classList.add('bg-yellow-200') : fila.classList.remove('bg-yellow-200');  
+    })
 })
